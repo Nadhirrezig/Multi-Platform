bef39b92a13eaa7ac78f3e3e11b916da0a395ce9
Nadhirrezig <rzignadhir56@gmail.com>
2025-03-17 16:04:05 +0100

    added cors headers

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
+const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server , 
+  {
+  cors: {
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST']
+  }
+});
 
 app.use(express.json());
+app.use(cors({origin: 'http://localhost:3000'}));
+// this will only work in local machine , other wise you can use * in origin
+// app.use(cors({origin: '*'}));
+// and ofcourse not only here you need to copy past your IP adress to page.tsx in nextjs webapp same as you did in this file
+// changing localhost:3000 to your IP adress and destinated port number
+// better to use a static ip adress for the testing so you dont need to change it every time you restart your router
+// with this in mind , now you can send and recieve message using different devices in the same network
+// congrats you built your own server
+// thats it for today 17/03
 
 let messages = [];
 app.get('/messages', (req, res) => {
@@ -21,7 +36,8 @@ app.post('/messages', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('Someone connected!');
+  const ip = socket.handshake.address;
+  console.log(ip,'Someone connected!');
 });
 
 server.listen(4000, () => {
