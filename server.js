9ca4697b7718edd8e79d650cb3f76d78eeb9992e
Nadhirrezig <rzignadhir56@gmail.com>
2025-03-22 13:20:30 +0100

    Server_workers update

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server , 
+const webpush = require('web-push');
+const io = new Server(
+  server , 
   {
-  cors: {
-  origin: [
-    'http://localhost:3000',
-    'http://192.168.1.11:3000',
-  ],
-  methods: ['GET', 'POST']
-  }
-});
+    cors: {
+    origin: [
+      'http://localhost:3000',
+      'http://192.168.1.11:3000',
+    ],
+    methods: ['GET', 'POST']
+    }
+  },
+  {connectionStateRecovery: {}} // cool shit when client disconnectes for a short delay for exemple switching from WIFI to 4/5G
+);
 
 app.use(express.json());
 app.use(cors({origin: ['http://localhost:3000' , 'http://192.168.1.11:3000']}));
@@ -25,8 +29,27 @@ app.use(cors({origin: ['http://localhost:3000' , 'http://192.168.1.11:3000']}));
 // with this in mind , now you can send and recieve message using different devices in the same network
 // congrats you built your own server
 // thats it for today 17/03
-
+const vapidkeys = {
+  PRIVATEKEY : "o_lLhx-b5XuWO0evNc_ngHs3u6oYAknthBe4CsizPLQ",
+  PUBLICKEY : "BME6S9g2Uqstk9_Y15xclo7mFV92_MH8oe2k2Z0IJGyP3yUSwYjz1rJveTEQVWrOPLC44R1RQooKchJNpYf3330"
+}
+webpush.setVapidDetails(
+  'mailto:rzignadhir56@gmail.com',
+  vapidkeys.PUBLICKEY,
+  vapidkeys.PRIVATEKEY
+);
 let messages = [];
+
+// kthor 3liya lcode brojla
+let subscriptions = [];
+app.post('/subscribe', (req, res) => {
+  const subscription = req.body;
+  console.log('Received subscription:', subscription);
+  subscriptions.push(subscription);
+  res.status(201).json({ status: 'Subscribed' });
+});
+
+
 app.get('/messages', (req, res) => {
   res.json(messages);
 });
@@ -37,11 +60,22 @@ app.post('/messages', (req, res) => {
   messages.push(message);
   io.emit('new-message', message);
   console.log('Broadcasted:', message);
+  const payload = JSON.stringify({
+    title: 'New Message',
+    body: message,
+  });
+  subscriptions.forEach((subscription) => {
+    webpush.sendNotification(subscription, payload)
+      .catch((err) => console.error('Push failed:', err));
+  });
   res.json({ status: 'Sent', message });
 });
 
 io.on('connection', (socket) => {
   console.log('Client connected:', socket.id);
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
 });
 server.listen(4000, () => {
   console.log('Backend running on http://localhost:4000');
